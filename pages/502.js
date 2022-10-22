4208d81b7abb369a30bdcfa6ee748c8a8cb814dc
Odyssey <odyssey346@disroot.org>
2022-10-22 19:32:11 +0200

    More specific in 502
    Signed-off-by: Odyssey <odyssey346@disroot.org>

--- pages/502.js
+++ pages/502.js
@@ -10,8 +10,8 @@ export default function FiveOhTwo() {
             <Navbar />
             <Heading m={4} color="red.300">502 Bad Gateway</Heading>
             <Text m={4}>Seems like something went wrong with the service you&apos;re using.</Text>
-            <Text m={4}>If this keeps happening/doesn&apos;t come back in at least an hour, please <Link colorScheme="blue" variant="solid" as={Link} href="mailto:hi@odyssey346.dev?subject=Your (enter service here) instance is down.">send me an E-mail. <ExternalLinkIcon /></Link></Text>
-            <Text m={4}>In the meantime, enjoy this cat GIF! </Text>
+            <Text m={4}>If this keeps happening, or the service doesn&apos;t come back in (at max) an hour, please <Link colorScheme="blue" variant="solid" as={Link} href="mailto:hi@odyssey346.dev?subject=Your (enter service here) instance is down.">send me an E-mail. <ExternalLinkIcon /></Link> I will respond during my day (UTC+2)</Text>
+            <Text m={4}>In the meantime, enjoy this cat GIF!</Text>
             <Image src="https://cataas.com/cat/gif" alt="A cat" m={4} rounded="lg" boxSize="sm" />
         </div>    
     )
